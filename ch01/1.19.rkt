9677feec5400278ced319ef1610ed86e7c48a864
Codevka <1724707236@qq.com>
2021-03-04 21:37:37 +0800

    [1.19][1.20] Finished.

--- ch01/1.19.rkt
+++ ch01/1.19.rkt
@@ -1,1 +1,12 @@
 #lang eopl
+
+(define list-set
+  (lambda (lst n x)
+    (if (null? lst)
+      '()
+      (if (zero? n)
+        (cons x (cdr lst))
+        (cons (car lst) (list-set (cdr lst) (- n 1) x))))))
+        
+; (list-set '(a b c d) 2 '(1 2))
+; (list-ref (list-set '(a b c d) 3 '(1 5 10)) 3)
--- ch01/1.20.rkt
+++ ch01/1.20.rkt
@@ -1,1 +1,15 @@
 #lang eopl
+
+(define count-occurrences
+  (lambda (s slist)
+    (if (null? slist)
+      0
+      (if (symbol? (car slist))
+        (if (equal? s (car slist))
+          (+ 1 (count-occurrences s (cdr slist)))
+          (count-occurrences s (cdr slist)))
+        (+ (count-occurrences s (car slist)) (count-occurrences s (cdr slist)))))))
+        
+; (count-occurrences 'x '((f x) y (((x z) x))))
+; (count-occurrences 'x '((f x) y (((x z) () x))))
+; (count-occurrences 'w '((f x) y (((x z) x))))
