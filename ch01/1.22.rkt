63f7d5e03c449b481dd95ce0d7c36e8a0a1c9dea
Codevka <1724707236@qq.com>
2021-03-04 22:33:16 +0800

    [1.22][1.23] Finished.

--- ch01/1.22.rkt
+++ ch01/1.22.rkt
@@ -1,1 +1,12 @@
 #lang eopl
+
+(define filter-in
+  (lambda (pred lst)
+    (if (null? lst)
+      '()
+      (if (pred (car lst))
+        (cons (car lst) (filter-in pred (cdr lst)))
+        (filter-in pred (cdr lst))))))
+    
+; (filter-in number? '(a 2 (1 3) b 7))
+; (filter-in symbol? '(a (b c) 17 foo))
--- ch01/1.23.rkt
+++ ch01/1.23.rkt
@@ -1,1 +1,17 @@
 #lang eopl
+
+(define list-index-sub
+  (lambda (pred lst index)
+    (if (null? lst)
+      #f
+      (if (pred (car lst))
+        index
+        (list-index-sub pred (cdr lst) (+ index 1))))))
+        
+(define list-index
+  (lambda (pred lst)
+    (list-index-sub pred lst 0)))
+
+; (list-index number? '(a 2 (1 3) b 7))
+; (list-index symbol? '(a (b c) 17 foo))
+; (list-index symbol? '(1 2 (a b) 3))
