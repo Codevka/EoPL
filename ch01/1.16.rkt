7cd7696f6732b606bd9ee22cafa7ab134571f9d3
Codevka <1724707236@qq.com>
2021-03-04 20:37:18 +0800

    [1.16][1.17] Finished.

---
+++ ch01/1.16.rkt
@@ -0,0 +1,11 @@
+#lang eopl
+      
+(define invert
+  (lambda (lst)
+    (if (null? lst)
+      '()
+      (cons 
+        (cons (cadr (car lst)) (car (car lst)))
+        (invert (cdr lst))))))
+        
+; (invert '((a 1) (a 2) (1 b) (2 b)))
--- ch01/1.17.rkt
+++ ch01/1.17.rkt
@@ -1,1 +1,12 @@
 #lang eopl
+
+(define down
+  (lambda (lst)
+    (map 
+      (lambda (elem)
+        (cons elem '()))
+      lst)))
+
+; (down '(1 2 3))
+; (down '((a) (fine) (idea)))
+; (down '(a (more (complicated)) object))
